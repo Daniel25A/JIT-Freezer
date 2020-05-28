6e6bc983adb8a85506dbc3068cc3fc6a3b7da16e
Oscar Gomez <37637868+Daniel25A@users.noreply.github.com>
2020-05-28 16:58:13 +0000

    algunas mejoras

--- MainCLI.cs
+++ MainCLI.cs
@@ -4,36 +4,39 @@ using System.Threading;
 
 namespace JITFreezer
 {
+
     internal class MainCLI
     {
+        [STAThread]
         private static void Main(string[] args)
         {
             Console.Title = "JIT Freezer - Made by ZrCulillo#1998";
             Console.ForegroundColor = ConsoleColor.Red;
-
-            if (args.Length < 1)
+            string ValueOfText= "\n[»] Drag & Drop your native files < 3.".Trim();
+            if (args.Length == 0)
             {
                 Utils.PrintLogo();
 
-                for (int i = 0; i < "\n[»] Drag & Drop your native files <3.".Length; i++)
+                for (int i = 0; i < ValueOfText.Length; i++)
                 {
-                    Console.Write("\n[»] Drag & Drop your native files <3."[i].ToString());
+                    Console.Write(ValueOfText[i].ToString());
                     Thread.Sleep(35);
                 }
                 Console.ReadLine();
                 return;
             }
-
+            Process freezedProcess = null;
             try
             {
                 int pID;
                 string freezedPath = args[0];
-                Process freezedProcess = new Process();
-                freezedProcess.StartInfo.FileName = freezedPath;
-                freezedProcess.StartInfo.CreateNoWindow = true;
-                freezedProcess.StartInfo.UseShellExecute = false;
+                freezedProcess  = new Process();
+                var InfoProcess = freezedProcess.StartInfo;
+                InfoProcess.FileName = freezedPath;
+                InfoProcess.CreateNoWindow = true;
+                InfoProcess.UseShellExecute = false;
                 freezedProcess.Start();
-                pID = freezedProcess.Id;
+                pID = freezedProcess.Id < 1 ? 0x4E : freezedProcess.Id;
                 while (true)
                 {
                     if (Utils.GetCLRModule(pID))
@@ -55,6 +58,11 @@ namespace JITFreezer
             {
                 Console.Clear();
                 Console.WriteLine("[x] Error: \n" + e);
+            }
+            finally
+            {
+                if (freezedProcess != null)
+                    freezedProcess.Dispose();
             }
         }
     }
--- Utils.cs
+++ Utils.cs
@@ -9,39 +9,37 @@ namespace JITFreezer
     {
         public static void PrintLogo()
         {
-            Colorful.Console.WriteLine();
+            Console.WriteLine(string.Empty);
             string[] JITFreezerArr = new string[]
-            {
+           {
                 "\t\t\t   __   _____  _____     ___             ",
                 "\t\t\t   \\ \\  \\_   \\/__   \\   / __\\ __ ___  ___ _______ _ __ ",
                 "\t\t\t    \\ \\  / /\\/  / /\\/  / _\\| '__/ _ \\/ _ \\_  / _ \\ '__|",
                 "\t\t\t /\\_/ /\\/ /_   / /    / /  | | |  __/  __// /  __/ |   ",
-                "\t\t\t \\___/\\____/   \\/     \\/   |_|  \\___|\\___/___\\___|_|   ",
-            };
+                "\t\t\t \\___/\\____/   \\/     \\/   |_|  \\___|\\___/___\\___|_|   "
+           };
             int r = 220;
             int g = 20;
             int b = 60;
-            for (int i = 0; i < JITFreezerArr.Length; i++)
-            {
-                Colorful.Console.WriteLine(JITFreezerArr[i], Color.FromArgb(r, g, b));
+            foreach (string Text in JITFreezerArr) {
+                Console.WriteLine(Text, Color.FromArgb(r, g, b));
                 g += 8;
                 b += 8;
             }
-            Colorful.Console.WriteLine();
+            Console.WriteLine(string.Empty);
         }
-
         /* Thanks to: 
          * https://stackoverflow.com/questions/71257/suspend-process-in-c-sharp 
          * https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.processmodulecollection?view=netframework-4.8
          */
 
-        public static bool GetCLRModule(int pID)
+        public static bool GetCLRModule(Int32 pID=0x2D)
         {
             ProcessModuleCollection pModuleCollection = Process.GetProcessById(pID).Modules;
-            for (int i = 0; i < pModuleCollection.Count; i++)
+            if (pModuleCollection.Count == 0) return false;
+            foreach(ProcessModule Module in pModuleCollection)
             {
-                if (pModuleCollection[i].ModuleName.ToLower() == "clr.dll")
-                    return true;
+                if (Module.ModuleName.ToLower() == "clr.dll") return true;
             }
             return false;
         }
@@ -61,7 +59,7 @@ namespace JITFreezer
         [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern bool CloseHandle(IntPtr handle);
 
-        public static void SuspendT(int pid)
+        public static void SuspendT(Int32 pid= 0x16D)
         {
             var pID = Process.GetProcessById(pid);
 
